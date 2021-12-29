44d83de4894f275051a8db05e58af6a107945e32
Adam Sillye <adam@berriesand.co>
2021-12-29 22:24:32 +0100

    script commit + doc

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# shelly-static-ip
-Reconfigure masses of Shelly devices to static IP
+# Script for setting static IPs to Shelly devices
+
+Reconfigure masses of Shelly devices to static IP. This is useful, if you want to set up your Shelly (https://shelly.cloud/) devices to use static IP addresses on your home network - so next time, if you look for them, they will be on the same address.
+
+Yes - the DHCP servers can be configured to give the same IP for a MAC address, but configuring that is more complex than just running this script and store the IPs in the Shelly devices.
+
+This script will scan your full local network for Shelly devices and if any of them are in DHCP mode, it will automatically pick a static IP address and assign it to them. 
+
+At the end, all DHCP Shelly devices will be reconfigured to a unique static IP.
+
+The script assumes a simple home network setup with 255.255.255.0 netmask. Scanning is done on the last number from 2 to 254.
+
+# Prerequisites
+Install Python 3
+
+Install required packages with
+```
+pip install -r requirements.txt
+```
+
+# Configuration
+There are no command line arguments, please modify the script directly.
+
+## GATEWAY_IP - Your Gateway IP
+Your home router address. This will be used to determine the subnet to scan and also this will be configured to all Shelly devices as a gateway.
+```
+GATEWAY_IP = "192.168.100.1"
+```
+The above setting will scan the ```192.168.100.2``` - ```192.168.100.254``` range for Shelly devices.
+
+## STATIC_IP_MIN, STATIC_IP_MAX: Static IP address range to use
+Range of the static IPs available to use in the same subnet.
+
+At home I've set up the router to assign DHCP addresses only in the range of 100-199, to above 200 all IPs are available for static IPs.
+```
+STATIC_IP_MIN = 200
+STATIC_IP_MAX = 254
+```
+
+# Running
+
+```
+python ./shelly-static-ip.py
+```
+The script will run in two passes:
+
+In the first pass with ```scanForShellys``` function floods the network with simple http requests and look for the Shelly ```/settings``` API. 
+
+If any meaningful response comes the script registers the device data and also saves into the ```shelly-ip-table.txt``` file.
+
+If no response comes, the script tries to PING the host and check for any network device there. 
+
+If there was no answer, it registers it as an available static IP (if there is a ping answer, it will skip that IP from the available static addresses).
+
+I added some basic multi threading, as it was quite slow sequentially, so the resuld order will be random.
+
+```
+INFO:root:Found: ip=192.168.100.202, mac=40F5202D3506, type=SHSW-1, name=Emeleti közlekedő lámpa, cloud=1, ipv4_method=static
+INFO:root:Found: ip=192.168.100.201, mac=40F5202D6287, type=SHSW-1, name=Lépcső lámpa, cloud=1, ipv4_method=static
+INFO:root:Found: ip=192.168.100.204, mac=8CAAB54807F7, type=SHSW-1, name=Garázs Lámpa, cloud=1, ipv4_method=static
+INFO:root:Found: ip=192.168.100.209, mac=C45BBE75F386, type=SHSW-1, name=None, cloud=1, ipv4_method=static
+INFO:root:Found: ip=192.168.100.212, mac=E868E7C571E3, type=SHPLG-S, name=None, cloud=1, ipv4_method=static
+```
+
+In the second pass with ```reconfigureShellys``` function, the script goes though sequentially all Shelly devices with DHCP and changes it to an automatically picked static IP. It picks the next available IP based on the first pass.
+
+```
+INFO:root:Reconfiguring Shelly with DHCP on IP 192.168.100.166 to new IP 192.168.100.212 with gateway 192.168.100.1
+```
+
---
+++ shelly-static-ip.py
@@ -0,0 +1,115 @@
+import threading
+import traceback
+import logging
+import requests
+from json.decoder import JSONDecodeError
+from ping3 import ping
+
+logging.basicConfig(level=logging.INFO)
+
+GATEWAY_IP = "192.168.100.1"
+STATIC_IP_MIN = 200
+STATIC_IP_MAX = 254
+
+lastDot = GATEWAY_IP.rfind(".")
+ipAddressBase = GATEWAY_IP[0:lastDot+1]
+threadLock = threading.Lock()
+availableForStaticIp = []
+dchpNeedToReconfigure = []
+
+def registerShellyFound(outputFile, ip, mac = "", type = "", ipv4_method ="", name = ""):
+    threadLock.acquire()
+    try:
+        outputFile.write(ip + '\t' + mac + '\t' + type + '\t' + ipv4_method + '\t' + str(name) + '\n')
+        if ipv4_method == "dhcp":
+            dchpNeedToReconfigure.append(ip)
+    finally:
+        threadLock.release()
+
+def detectDevice(ipLast):
+    ip = ipAddressBase + str(ipLast)
+    if STATIC_IP_MIN < ipLast & ipLast < STATIC_IP_MAX:
+        logging.debug('No Shelly on IP %s, pinging IP to check availability...', ip)
+        pingResult = ping(ip)
+        if pingResult == False:
+            logging.debug("No device on IP %s, registering as available static IP", ip)
+            availableForStaticIp.append(ipLast)
+        else:
+            logging.debug('Network device detected on IP %s, ping in %f sec.', ip, pingResult)
+            return
+
+def detectShelly(ipLast, outputFile):
+    try:
+        ip = ipAddressBase + str(ipLast)
+        logging.debug('Checking for Shelly at IP %s...', ip)
+        url = "http://" + ip + "/settings"
+        response = requests.get(url, timeout=5)
+
+        if response.status_code != 200:
+            detectDevice(ipLast)
+            return
+
+        json = response.json()
+
+        device = json["device"]
+        cloud = json["cloud"]
+        cloud_enabled = cloud["enabled"]
+        name = json["name"]
+        mac = device["mac"]
+        type = device["type"]
+        wifi_sta = json["wifi_sta"]
+        ipv4_method = wifi_sta["ipv4_method"]
+
+        logging.info("Found: ip=%s, mac=%s, type=%s, name=%s, cloud=%d, ipv4_method=%s", ip, mac, type, name, cloud_enabled, ipv4_method)
+        registerShellyFound(outputFile, ip, mac, type, ipv4_method, name)
+    except JSONDecodeError:
+        return
+    except AttributeError:
+        return
+    except requests.ConnectionError as error:
+        detectDevice(ipLast)
+        return
+
+def configureStaticIp(currentIp, newIp, gatewayIp):
+    try:
+        # example: http://192.168.100.165/settings/sta?ipv4_method=static&ip=192.168.100.208&netmask=255.255.255.0&gateway=192.168.100.1
+        logging.info("Reconfiguring Shelly with DHCP on IP %s to new IP %s with gateway %s", currentIp, newIp, gatewayIp)
+        url = "http://" + currentIp + "/settings/sta?ipv4_method=static&ip=" + newIp + "&netmask=255.255.255.0&gateway=" + gatewayIp
+        response = requests.get(url, timeout=5)
+        if response.status_code != 200:
+            logging.error("Error reconfiguring %s error code %d", currentIp, response.status_code)
+            return
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        return
+
+
+def scanForShellys():
+    ipTableFile = open("shelly-ip-table.txt", "w", encoding="utf-8")
+    threads = []
+
+    for c in range(2, 254):
+        t = threading.Thread(target=detectShelly, args=(c, ipTableFile))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+    ipTableFile.close()
+    availableForStaticIp.sort()
+    dchpNeedToReconfigure.sort()
+    
+
+def reconfigureDhcpShellys():
+    for ipToReconfigure in dchpNeedToReconfigure:
+        if availableForStaticIp.count == 0:
+            logging.error("No more static IP slot available for %s. Stopping.", ipToReconfigure)
+            break
+
+        staticIpLast = availableForStaticIp.pop(0)
+        staticIp = ipAddressBase + str(staticIpLast)
+        configureStaticIp(ipToReconfigure, staticIp, GATEWAY_IP)
+
+
+scanForShellys()
+reconfigureDhcpShellys()
